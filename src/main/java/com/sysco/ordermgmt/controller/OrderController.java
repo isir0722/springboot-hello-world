5f7d502db5c3eb8855f71e4f707e4dbd8442d935
isir0722 <isir0722@sysco.com>
2018-08-07 16:10:11 +0530

    Initialize the Spring Project

---
+++ src/main/java/com/sysco/ordermgmt/controller/OrderController.java
@@ -0,0 +1,17 @@
+package com.sysco.ordermgmt.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+public class OrderController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String index(HttpServletResponse response) {
+        return "Hello World!";
+    }
+
+}
