d8c9d9ed144f23c6316c3f270e1527a89b6f6a2a
isir0722 <isir0722@sysco.com>
2018-08-10 14:45:51 +0530

    Unit tests added

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+b
+2src/main/java/com/sysco/ordermgmt/Application.java,c/6/c6ed6bceaba1e8eebb7da728a317581f1770c312
+q
+Asrc/main/java/com/sysco/ordermgmt/controller/OrderController.java,9/e/9e0589a81d39a727033b89fe9b9eb5f18cb5d948
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
--- src/main/java/com/sysco/ordermgmt/controller/OrderController.java
+++ src/main/java/com/sysco/ordermgmt/controller/OrderController.java
@@ -1,19 +1,16 @@
 package com.sysco.ordermgmt.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class OrderController {
 
-    private static String nullObject = null;
-
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String index(HttpServletResponse response) {
-        return "Hello World! test 1234";
+    @RequestMapping("/")
+    public @ResponseBody
+    String greeting() {
+        return "Hello World";
     }
 
 }
